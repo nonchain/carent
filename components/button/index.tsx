f0c27aa6967b4b3f2ff651fa8093b59bc26f6c25
progali007 <code.progali@gmail.com>
2023-06-28 10:48:13 +0330

    :sparkles: (button): Define new props for button

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,9 +3,9 @@
 import Image from "next/image";
 import { ButtonProps } from "./types/index.type";
 
-function Button({title, containerStyles, onClick}: ButtonProps) {
+function Button({title, containerStyles, type="button", onClick}: ButtonProps) {
   return (
-    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={onClick}>
+    <button disabled={false} type={type} className={`custom-btn ${containerStyles}`} onClick={onClick}>
       <span className={`flex-1`}>{title}</span>
     </button>
   );
--- components/button/types/index.type.ts
+++ components/button/types/index.type.ts
@@ -3,5 +3,6 @@ import { MouseEventHandler } from "react";
 export interface ButtonProps {
   title: string;
   containerStyles?: string;
+  type?: "button" | "submit" | "reset" | undefined;
   onClick?: MouseEventHandler<HTMLButtonElement> 
 }
