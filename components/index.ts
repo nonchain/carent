481b7a9ce889738d61cacd2daa37a0a50028b610
progali007 <code.progali@gmail.com>
2023-06-28 11:23:49 +0330

    :wrench: (components): Implement some components

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,6 @@
 import HeroSection from "./hero";
 import Button from "./button";
 import Navbar from "./navbar";
+import Footer from "./footer";
 
-export {
-  HeroSection,
-  Button,
-  Navbar,
-}
+export { HeroSection, Button, Navbar, Footer, };
