8ee4b84034d5e3cb20b0f63cc8dd990ed8569396
progali007 <code.progali@gmail.com>
2023-07-05 15:59:23 +0330

    :sparkles: (button): Add right icon element

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,10 +3,15 @@
 import Image from "next/image";
 import { ButtonProps } from "./types/index.type";
 
-function Button({title, containerStyles, type="button", onClick}: ButtonProps) {
+function Button({ title, containerStyles, type = "button", onClick, rightIcon, isDisable = false }: ButtonProps) {
   return (
-    <button disabled={false} type={type} className={`custom-btn ${containerStyles}`} onClick={onClick}>
+    <button disabled={isDisable} type={type} className={`custom-btn ${containerStyles}`} onClick={onClick}>
       <span className={`flex-1`}>{title}</span>
+      {rightIcon ? (
+        <div className="w-6 h-6 relative">
+          <Image src={`/${rightIcon}`} alt="right-icon" fill className="object-contain" />
+        </div>
+      ) : null}
     </button>
   );
 }
