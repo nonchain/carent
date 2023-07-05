470b359b6742527eeaf5460e97a4c6478f367820
progali007 <code.progali@gmail.com>
2023-07-05 13:43:37 +0330

    :sparkles: (page): Implement search bar section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { HeroSection } from '@/components'
-import Image from 'next/image'
+import { Filter, HeroSection, SearchBar } from "@/components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <HeroSection />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <Filter title="fuel"/>
+            <Filter title="year"/>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
