563b06860cf781769f19a537924fff3449f69d5c
progali007 <code.progali@gmail.com>
2023-07-11 14:58:38 +0330

    :wrench: (components): Implement new component

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,9 @@ import HeroSection from "./hero";
 import Button from "./button";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Filter from "./filter";
+import SearchBar from "./search-bar";
+import CarCard from "./car-card";
+import CarDetailsCard from "./car-details-card";
 
-export { HeroSection, Button, Navbar, Footer, };
+export { HeroSection, Button, Navbar, Footer, Filter, SearchBar, CarCard, CarDetailsCard };
